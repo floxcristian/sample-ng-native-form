e769d64f0c7101ff9d5bf7237e8c281aae0da432
Cristian Flores <cristianflores.ee@gmail.com>
2022-07-02 14:11:15 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-native-form';
+  @ViewChild('imedFormEl') imedFormElement!: ElementRef;
+
+  actionLink: string;
+  imedForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.actionLink = 'https://interfaz4pre.bonoelectronico.cl/login.php';
+    this.imedForm = this.fb.group({
+      NroTransac: '',
+      NroAuditoria: '',
+    });
+  }
+
+  executeForm(): void {
+    this.imedFormElement.nativeElement.submit();
+  }
 }
